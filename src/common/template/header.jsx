36b54edfcb7a68122817a1b9ad6c81f602950aab
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-23 14:28:27 -0300

    Configurando o projeto e criando o header

---
+++ src/common/template/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default props => (
+    <header className="main-header">
+        <a href="/#/" className="logo">
+            <span className="logo-mini"><b>F</b>C</span>
+            <span className="logo-lg">
+                <i className="fa fa-money"></i>
+                <b> Financial</b> Control
+            </span>
+        </a>
+        <nav className="navbar navbar-statis-top">
+            <a href className="sidebar-toggle" data-toggle="offcanvas"></a>
+        </nav>
+    </header>
+)
---
+++ src/main/App.js
@@ -0,0 +1,9 @@
+import '../common/template/dependencies'
+import React from "react";
+import Header from '../common/template/header';
+
+export default props => (
+  <div className="wrapper">
+    <Header/>
+  </div>
+)
